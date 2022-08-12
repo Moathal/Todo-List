813b2388ff2112557429659b8af348de8c7cd11a
Moathal <moathal.s.kachi@gmail.com>
2022-08-12 16:32:32 +0300

    Add eventListeners to add task
    Add eva function to add new task
    
    Add evenListener for clicking any of the items inside a task
    
    Add function for changing the description
    
    Add function for deleting a task
    
    add function to rearrange array & fix order after deleting

--- src/index.js
+++ src/index.js
@@ -1,27 +1,71 @@
 import './style.css';
-import Task from './modules/task.js';
 import MethodsTasks from './modules/methods.js';
 import { descriptFocusIn, descriptFocusOut } from './modules/interact.js';
 
 const methods = new MethodsTasks();
 const tasksList = document.querySelector('.tasks');
+const newDescription = document.getElementById('i--1');
+const submitBtn = document.getElementById('a--1');
 
-let todo = new Task('bench press', false, 0);
-methods.addTask(todo);
-todo = new Task('jump 20 times', false, 2);
-methods.addTask(todo);
-todo = new Task('jog for 20 minutes', true, 1);
-methods.addTask(todo);
+if (localStorage.getItem('tasks')) methods.tasks = JSON.parse(localStorage.getItem('tasks'));
 
 methods.resetList(tasksList);
 
-const checkboxes = document.querySelectorAll('.checkbox');
-const textareas = document.querySelectorAll('.description');
+newDescription.addEventListener('keydown', (e) => {
+  const description = newDescription.value;
+  e.preventDefault();
+  if (description.length > 0 && e.code === 'Enter') {
+    methods.addTask(description, false, methods.tasks.length);
+    methods.resetList(tasksList);
+    localStorage.setItem('tasks', JSON.stringify(methods.tasks));
+    newDescription.value = '';
+  }
+});
 
-checkboxes.forEach((checkbox) => checkbox.addEventListener('click', () => {
-  methods.clickCheckbox(checkbox.id);
-}));
+submitBtn.addEventListener('click', () => {
+  const description = newDescription.value;
+  if (description.length > 0) {
+    methods.addTask(description, false, methods.tasks.length);
+    methods.resetList(tasksList);
+    newDescription.value = '';
+    localStorage.setItem('tasks', JSON.stringify(methods.tasks));
+  }
+});
 
-textareas.forEach((textarea) => textarea.addEventListener('focusin', () => descriptFocusIn(textarea.id)));
+tasksList.addEventListener('click', (e) => {
+  const element = e.target.id.substring(0, 2);
+  let idIndex = e.target.id.substring(2, e.target.id.length);
+  if (element === 'c-') {
+    methods.clickCheckbox(e.target.id);
+    localStorage.setItem('tasks', JSON.stringify(methods.tasks));
+  } else if (element === 'b-' || element === 'tr' || element === 'mo') {
+    if (element === 'mo' || element === 'tr') idIndex = e.target.id.substring(5, e.target.id.length);
+    const task = document.getElementById(`d-${idIndex}`);
+    methods.removeTask(tasksList, idIndex, task);
+    methods.resetList(tasksList);
+    localStorage.setItem('tasks', JSON.stringify(methods.tasks));
+  }
+});
 
-textareas.forEach((textarea) => textarea.addEventListener('focusout', () => descriptFocusOut(textarea.id)));
+tasksList.addEventListener('focusin', (e) => {
+  const element = e.target.id.substring(0, 2);
+  if (element === 't-') {
+    descriptFocusIn(e.target.id);
+  }
+});
+
+tasksList.addEventListener('focusout', (e) => {
+  const element = e.target.id.substring(0, 2);
+  if (element === 't-') {
+    descriptFocusOut(e.target.id);
+  }
+});
+
+tasksList.addEventListener('change', (e) => {
+  const element = e.target.id.substring(0, 2);
+  const idIndex = e.target.id.substring(2, e.target.id.length);
+  if (element === 't-') {
+    methods.tasks[idIndex].description = e.target.value;
+    localStorage.setItem('tasks', JSON.stringify(methods.tasks));
+  }
+});
--- src/modules/methods.js
+++ src/modules/methods.js
@@ -1,9 +1,12 @@
+import Task from './task.js';
+
 export default class methodsTasks {
   constructor() {
     this.tasks = [];
   }
 
   resetList(tasksList) {
+    tasksList.innerHTML = '';
     this.tasks.forEach((task, i) => {
       const div = document.createElement('div');
       div.classList.add('py-1');
@@ -12,41 +15,58 @@ export default class methodsTasks {
       div.innerHTML = `
         <input id="c-${i}" class="checkbox" type="checkbox">
         <div class="text">
-        <textarea id="t-${i}" class="fs-4 fw-light description">${task.description}</textarea>
+        <input id="t-${i}" class="fs-4 fw-light description" value="${task.description}">
         </div>
-        <button id="b-${i}" class="move-delete move"><i id="trash-${i}" class="fa-regular fa-trash-can fa-xl hidden"></i>
+        <button type="button" id="b-${i}" class="move-delete move"><i id="trsh-${i}" class="fa-regular fa-trash-can fa-xl hidden"></i>
         <i id="move-${i}" class="fa-solid fa-ellipsis-vertical show"></i>
         </button>`;
       div.style.order = task.index;
       tasksList.appendChild(div);
       if (task.completed) {
         const checkbox = document.getElementById(`c-${i}`);
-        const textarea = document.getElementById(`t-${i}`);
+        const input = document.getElementById(`t-${i}`);
         checkbox.setAttribute('checked', '');
-        textarea.classList.add('text-decoration-line-through');
-        textarea.classList.add('text-secondary');
+        input.classList.add('text-decoration-line-through');
+        input.classList.add('text-secondary');
       }
     });
   }
 
-  addTask(task) {
-    this.tasks.push(task);
+  addTask(description, completed, index) {
+    let object;
+    if (index > 0) {
+      object = new Task(description, completed, index);
+    } else {
+      object = new Task(description, completed, index + 1);
+    }
+    this.tasks.push(object);
   }
 
   clickCheckbox(id) {
     const index = id.replace('c-', '');
     const checkbox = document.getElementById(id);
-    const textarea = document.getElementById(`t-${index}`);
+    const input = document.getElementById(`t-${index}`);
     if (checkbox.hasAttribute('checked')) {
-      textarea.classList.remove('text-secondary');
-      textarea.classList.remove('text-decoration-line-through');
+      input.classList.remove('text-secondary');
+      input.classList.remove('text-decoration-line-through');
       checkbox.removeAttribute('checked');
       this.tasks[index].completed = false;
     } else {
-      textarea.classList.add('text-decoration-line-through');
-      textarea.classList.add('text-secondary');
+      input.classList.add('text-decoration-line-through');
+      input.classList.add('text-secondary');
       checkbox.setAttribute('checked', '');
       this.tasks[index].completed = true;
     }
+  }
+
+  removeTask(tasksList, idIndex, task) {
+    this.tasks.splice(idIndex, 1);
+    tasksList.removeChild(task);
+    this.rearrangeOrder();
+  }
+
+  rearrangeOrder() {
+    this.tasks = this.tasks.sort((a, b) => a.index - b.index);
+    for (let i = 0; i < this.tasks.length; i += 1) this.tasks[i].index = i + 1;
   }
 }
